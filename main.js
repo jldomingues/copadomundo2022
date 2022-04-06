d1e544c5adbe2b67d84122fb03c5a5631cc6d78f
José Lucas Domingues <97459319+jldomingues@users.noreply.github.com>
2022-04-06 12:26:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+function grupoa(){
+  var pga=document.getElementById('ga')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoA</h1><th><td><img src="img/catar.png" width=90px><br>Catar</td></tr><th><td><img src="img/equador.png"width=90px>Equador</td></tr><th><td><img src="img/senegal.png"width=90px>Senegal</td></tr><th><td><img src="img/holanda.png"width=90px>Holanda</td></tr>')
+  
+}
+function grupob(){
+  var pga=document.getElementById('gb')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoB</h1><br><th><td><img src="img/inglaterra.png" width=90px><br>Inglaterra</td></tr><br><th><td><img src="img/ira.png"width=90px><br>Irã</td></tr><th><td><img src="img/eua.png"width=90px>Estados Unidos</td></tr><th><td><img src="img/repeur.png"width=90px>Repescagem Europa</td></tr>')
+  
+}
+function grupoc(){
+  var pga=document.getElementById('gc')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoC</h1><th><td><img src="img/argentina.png" width=90px><br>Argentina</td></tr><th><td><img src="img/arabia.png"width=90px><br>Arábia Saudita</td></tr><th><td><img src="img/mexico.png"width=90px><br>México</td></tr><th><td><img src="img/polonia.png"width=90px><br>Polônia</td></tr>')
+  
+}
+function grupod(){
+  var pga=document.getElementById('gd')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoD</h1><th><td><img src="img/franca.png" width=90px><br>França</td></tr><th><td><img src="img/inter.png"width=90px>Repescagem Intercontinental</td></tr><th><td><img src="img/dinamarca.png"width=90px>Dinamarca</td></tr><th><td><img src="img/tunisia.png"width=90px><br>Tunísia</td></tr>')
+  
+}
+function grupoe(){
+  var pga=document.getElementById('ge')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoE</h1><th><td><img src="img/espanha.png" width=90px><br>Espanha</td></tr><th><td><img src="img/inter.png"width=90px>Repescagem Intercontinental2</td></tr><th><td><img src="img/alemanha.png"width=90px>Alemanha</td></tr><th><td><img src="img/japao.png"width=90px><br>Japão</td></tr>')
+  
+}
+function grupof(){
+  var pga=document.getElementById('gf')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoF</h1><th><td><img src="img/belgica.png" width=90px><br>Bélgica</td></tr><th><td><img src="img/canada.png"width=90px><br>Canadá</td></tr><th><td><img src="img/marrocos.png"width=90px>Marrocos</td></tr><th><td><img src="img/croacia.png"width=90px><br>Croácia</td></tr>')
+  
+}
+
+function grupog(){
+  var pga=document.getElementById('gg')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoG</h1><th><td><img src="img/brasil.png" width=90px><br>Brasil</td></tr><th><td><img src="img/servia.png"width=90px><br>Sérvia</td></tr><th><td><img src="img/suica.png"width=90px><br>Suíça</td></tr><th><td><img src="img/camaroes.png"width=90px>Camarões</td></tr>')
+  
+}
+function grupoh(){
+  var pga=document.getElementById('gh')
+  var pres=document.getElementById('res')
+  pres.innerHTML=(' <table><h1>&nbsp;GrupoH</h1><th><td><img src="img/portugal.png" width=90px><br>Portugal</td></tr><th><td><img src="img/gana.png"width=90px><br>Gana</td></tr><th><td><img src="img/uruguai.png"width=90px>Uruguai</td></tr><th><td><img src="img/coreiadosul.png"width=90px><br>Coreia do Sul</td></tr>')
+  
+}
